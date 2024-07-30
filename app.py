eb627fa88b18956981f7a2c6bfdf1c829cc5318d
JEAN LUCAS LUQUETTI SILVA <JEANLS@suzano.com.br>
2024-07-30 10:23:39 -0300

    remove ton from UNBC

--- app.py
+++ app.py
@@ -53,19 +53,24 @@ def main():
     col1, col2, col3, col4, col5, col6 = st.columns([0.75, 1, 1, 0.8, 1, 2])
     with col1:
       csv_rate_geo, csv_rate_geo_cost_group = rate_geo(model=df, unity=unity)
-      st.download_button(label='Rate Geo', data=csv_rate_geo, file_name='rate_geo.csv', mime='text/csv')
+      if st.download_button(label='Rate Geo', data=csv_rate_geo, file_name='rate_geo.csv', mime='text/csv'):
+        st.toast('Rate Geo table downloaded!')
     with col2:
-      st.download_button(label='Rate Geo Cost Group', data=csv_rate_geo_cost_group, file_name='rate_geo_cost_group.csv', mime='text/csv')
+      if st.download_button(label='Rate Geo Cost Group', data=csv_rate_geo_cost_group, file_name='rate_geo_cost_group.csv', mime='text/csv'):
+        st.toast('Rate Geo Cost Group table downloaded!')
     with col3:
       csv_rate_geo_cost_viagem = rate_geo_cost_viagem(model=df, unity=unity)
-      st.download_button(label='Rate Geo Cost (viagem)', data=csv_rate_geo_cost_viagem, file_name='rate_geo_cost_viagem.csv', mime='text/csv')
+      if st.download_button(label='Rate Geo Cost (viagem)', data=csv_rate_geo_cost_viagem, file_name='rate_geo_cost_viagem.csv', mime='text/csv'):
+        st.toast('Rate Geo Cost (viagem) table downloaded!')
     with col4:
-      csv_rate_geo_cost_ton = rate_geo_cost_ton(model=df, unity=unity, min_cost=True)
-      if unity == 'UNC':
-        with col5:
-          min_cost_flag = st.checkbox('min_cost')
-          csv_rate_geo_cost_ton = rate_geo_cost_ton(model=df, unity=unity, min_cost=min_cost_flag)
-      st.download_button(label='Rate Geo Cost (ton)', data=csv_rate_geo_cost_ton, file_name='rate_geo_cost_ton.csv', mime='text/csv')
+      if unity != 'UNBC':
+        csv_rate_geo_cost_ton = rate_geo_cost_ton(model=df, unity=unity, min_cost=True)
+        if unity == 'UNC':
+          with col5:
+            min_cost_flag = st.checkbox('min_cost')
+            csv_rate_geo_cost_ton = rate_geo_cost_ton(model=df, unity=unity, min_cost=min_cost_flag)
+        if st.download_button(label='Rate Geo Cost (ton)', data=csv_rate_geo_cost_ton, file_name='rate_geo_cost_ton.csv', mime='text/csv'):
+          st.toast('Rate Geo Cost (ton) table downloaded!')
 
     ## DataFrame view ##
     st.write('')
