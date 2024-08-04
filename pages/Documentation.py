47d4a3fb477c87f10ac88ff4cdc9abbce84085ce
Jean-Lucas <jean.lucasls96@gmail.com>
2024-08-03 21:57:13 -0300

    doc

--- pages/Documentation.py
+++ pages/Documentation.py
@@ -18,12 +18,12 @@ st.header(body='📜 Documentation 🗃️', divider='green');  st.text('')
 
 col1, col2 = st.columns([4, 1])
 with col1:
-  st.subheader('📑 O arquivo carregado deve conter as colunas do modelo (disponível para download 📥)')
+  st.subheader('📑 Utilização da tabela modelo (disponível para download 📥)')
 with col2:
   model_excel = to_excel(model)
   if st.download_button(label='📥 model.xlsx', data=model_excel, file_name='model.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', help='Download da tabela modelo'):
     st.toast('Model table downloaded!')
-st.markdown('📌 As colunas são ORIGEM | DESTINO | SAP | VEICULO | FRETE, e acusarão erro caso não estejam neste formato')
+st.markdown('📌 As colunas são :green-background[ORIGEM | DESTINO | SAP | VEICULO | FRETE], e :red-background[acusarão erro] caso não estejam neste formato')
 st.dataframe(model, hide_index=True,
             column_config={
             'ORIGEM': st.column_config.TextColumn(label='📍 ORIGEM'),
@@ -32,10 +32,11 @@ st.dataframe(model, hide_index=True,
             'VEICULO': st.column_config.TextColumn(label='🚚 VEICULO'),
             'FRETE': st.column_config.NumberColumn(label='💸 FRETE', format='%.2f R$')
             })
+
 st.divider()
 
-st.subheader('📑 Os campos de ORIGEM podem ser escritos tanto no formato SAP quanto OTM e devem constar no dicionário')
-st.markdown('📌 Tanto FSCB quanto FAB_SUZ_1101 irão resultar em FAB_SUZ_1101')
+st.subheader('📑 Formatos aceitos para os campos de ORIGEM')
+st.markdown('📌  Podem ser escritos no :green-background[formato do SAP ou OTM] e devem constar no dicionário. Exemplo: Tanto FSCB quanto FAB_SUZ_1101 irão resultar em FAB_SUZ_1101')
 col1, col2, col3, col4 = st.columns([0.75, 0.25, 1, 3])
 with col1:
   st.dataframe(model['ORIGEM'], hide_index=True, column_config={'ORIGEM': st.column_config.TextColumn(label='📍 ORIGEM')})
@@ -43,8 +44,22 @@ with col2:
   st.text('');  st.text('');  st.markdown('➡️➡️')
 with col3:
   st.dataframe(model2, hide_index=True)
+st.markdown('📌 :green-background[Origens incorretas] e/ou não listadas no dicionário :red-background[serão excluídas das tabelas geradas] para evitar cadastros incorretos, e :blue-background[evidenciadas em uma tabela a parte] mostrando os índices das linhas incorretas')
+col1, col2, col3, col4 = st.columns([1.25, 0.25, 2, 4])
+with col1:
+  st.dataframe(model5)
+with col2:
+  st.text('');  st.text('');  st.text('');  st.text('')
+  st.markdown('➡️➡️')
+with col3:
+  st.markdown('❗ Not-mapped origins')
+  st.dataframe(model5.loc[[1, 3]], column_config={'📍 ORIGEM': st.column_config.TextColumn(label='⚠️ ORIGEM')})
+st.markdown('📌 :green-background[Consultar o dicionário das origens] com o time de SupriLog e :blue-background[solicitar acréscimo] caso não haja registro de alguma em específico')
 
-st.markdown('📌 Rate Geo e Rate Geo Cost Group terão as :green-background[duplicatas removidas] para :red-background[evitar o cadastro de dois ID iguais]')
+st.divider()
+
+st.subheader("📑 Tratamento de duplicatas nos ID's")
+st.markdown("📌 Rate Geo e Rate Geo Cost Group terão as :green-background[duplicatas removidas] para :red-background[evitar o cadastro de dois ID's iguais]")
 col1, col2, col3, col4, col5 = st.columns([1.25, 0.2, 1, 0.2, 1.25])
 with col1:
   st.dataframe(model3, hide_index=True, column_config={'💸 FRETE': st.column_config.NumberColumn(format='%.2f R$')})
@@ -61,14 +76,11 @@ with col5:
   st.text('');  st.text('')
   st.dataframe(model4, hide_index=True)
 
-st.markdown('📌 :green-background[Origens incorretas] e/ou não listadas no dicionário serão :red-background[excluídas das tabelas geradas] para evitar cadastros incorretos, e :green-background[evidenciadas] em uma tabela a parte :green-background[mostrando os índices das linhas incorretas]')
-col1, col2, col3, col4 = st.columns([1.25, 0.25, 2, 4])
+st.divider()
+
+st.subheader('📑 Especificidade das tabelas por Unidade de Negócio')
+st.markdown('📌 As tabelas geradas contemplam a as :green-background[particularidades de cada Unidade de Negócio], sendo possível de :blue-background[selecionar a UN desejada]')
+col1, col2 = st.columns([3, 7])
 with col1:
-  st.dataframe(model5)
-with col2:
-  st.text('');  st.text('');  st.text('');  st.text('')
-  st.markdown('➡️➡️')
-with col3:
-  st.markdown('❗ Not-mapped origins')
-  st.dataframe(model5.loc[[1, 3]], column_config={'📍 ORIGEM': st.column_config.TextColumn(label='⚠️ ORIGEM')})
-st.markdown('📌 Consultar o dicionário das origens com o time de SupriLog e solicitar acréscimo caso não encontre alguma')
+  st.selectbox('Escolha a Unidade de Negócio:', ('UNPE', 'UNBC', 'UNC'))
+st.markdown('📌 :blue-background[Procurar o responsável da UN] pelos cadastros no OTM :green-background[em caso de dúvidas]')
