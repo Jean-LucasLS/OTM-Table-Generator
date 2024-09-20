adf5f7136f1062fa465fa26e063dcd78a3d3ac04
Jean-Lucas Luquetti Silva <jean.lucasls96@gmail.com>
2024-09-20 10:57:21 -0300

    multicoletaviagem

--- otm_generator.py
+++ otm_generator.py
@@ -36,4 +36,5 @@ def rate_geo_cost_ton(model, unity, min_cost=True):
 
 def rate_geo_cost_viagem(model, unity):
   rate_geo_cost_viagem = df_rgcv_cols(model, unity)
-  return rate_geo_cost_viagem.to_csv(index=False)
+
+  return rate_geo_cost_viagem#.to_csv(index=False)
