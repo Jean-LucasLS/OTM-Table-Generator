5ef0aa7cf353f1a70567b1d88d32de7188bf7998
Jean-Lucas <jean.lucasls96@gmail.com>
2026-05-23 12:15:19 -0300

    update

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ st.set_page_config(page_title='OTM Table Generator', page_icon='🤖', layout='w
 
 pg = st.navigation([
     st.Page('OTM_Table_Generator.py', title='OTM Table Generator', icon='⚙️'),
-    st.Page('pages/Documentation.py',  title='Documentation',        icon='📜'),
+    st.Page('pages/Documentation.py', title='Documentation',       icon='📜'),
 ])
 pg.run()
